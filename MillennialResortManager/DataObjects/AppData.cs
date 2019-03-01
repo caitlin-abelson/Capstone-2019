64e0b85842665107acbff9cd1db9fd2b573b604a
adelabeef <au.delaney@gmail.com>
2019-03-01 14:26:51 -0600

    Divine cleansing of the mighty repository

--- MillennialResortManager/DataObjects/AppData.cs
+++ MillennialResortManager/DataObjects/AppData.cs
@@ -12,11 +12,6 @@ namespace DataObjects
 		/// This is the top most level of the application, and is set from the App.xaml.cs file at runtime. To set this,
 		/// create an event handler for the "startup" event on the "application" markup of the app.xaml file. Then, in
 		/// app.xaml.cs method that you just created, add these lines of code:
-		/// string topFolder = "";
-		/// string[] pathToDebug = AppDomain.CurrentDomain.BaseDirectory.Split('\\');
-		/// for (int i = 0; i < pathToDebug.Length - 4; i++)
-		/// {topFolder += pathToDebug[i] + '\\';}
-		/// DataObjects.AppData.TopRuntimeDirectory = topFolder;
 		/// </summary>
 		public static string TopRuntimeDirectory { get; set; }
 	}
--- MillennialResortManager/Presentation/App.xaml.cs
+++ MillennialResortManager/Presentation/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DataObjects;
 
 namespace Presentation
 {
@@ -12,7 +13,14 @@ namespace Presentation
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-    {
-
-    }
+	{
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			string topFolder = "";
+			string[] pathToDebug = AppDomain.CurrentDomain.BaseDirectory.Split('\\');
+			for (int i = 0; i < pathToDebug.Length - 4; i++)
+			{topFolder += pathToDebug[i] + '\\';}
+			DataObjects.AppData.TopRuntimeDirectory = topFolder;
+		}
+	}
 }
