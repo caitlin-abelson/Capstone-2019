5bf5d85648a8146a1dab987d85109f80e40aabf9
tektechnologies <info@tektechnologies.com>
2019-03-19 18:16:28 -0500

    add scroll feature to header

--- MillennialResortManager/MillennialResortWebSite/App_Start/BundleConfig.cs
+++ MillennialResortManager/MillennialResortWebSite/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace MillennialResortWebSite
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                      "~/Scripts/main.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
