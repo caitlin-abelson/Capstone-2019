d3519aafe1f294ca234cabc91adea946a6ca8dfd
MHill91 <kingmuyo@gmail.com>
2019-03-29 14:49:17 -0500

    addTestData.bat

--- MillennialResortManager/Database Items/addTestData.bat
+++ MillennialResortManager/Database Items/addTestData.bat
@@ -4,11 +4,9 @@ rem Adds developer written test data to the Database.
 rem 2019-03-22
 
 rem This is the name you change. _____.sql Your file name goes in the blank.
-<<<<<<< HEAD
-sqlcmd -S localhost -E -i test_data_scripts/3-29-2019PhillipHansen.sql
-=======
+
 sqlcmd -S localhost -E -i test_data_scripts/matthew-hill-update-3.29.19.sql
->>>>>>> dev
+
 
 ECHO .
 ECHO If no errors appeared, your test script and/or data was inserted
