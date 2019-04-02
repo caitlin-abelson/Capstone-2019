7abb6f361f2026d4239a012125a2d78c25a1db44
tektechnologies <info@tektechnologies.com>
2019-04-02 10:05:19 -0500

    add JqueryUI to scripts

---
+++ MillennialResortManager/MillennialResortWebSite/Controllers/ReservationController.cs
@@ -0,0 +1,18 @@
+﻿using MillennialResortWebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MillennialResortWebSite.Controllers
+{
+    public class ReservationController : Controller
+    {
+        // GET: Reservation
+        public ActionResult Index(ReservationSearchModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MillennialResortManager/MillennialResortWebSite/Models/ReservationSearchModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MillennialResortWebSite.Models
+{
+    public class ReservationSearchModel
+    {
+
+        //nullable 
+        //Start Date
+        //End Date
+        
+    }
+}
