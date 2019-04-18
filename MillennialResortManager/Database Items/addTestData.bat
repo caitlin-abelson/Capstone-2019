e0bea2d6b41c4206acc36f799877f17b6870c705
tektechnologies <info@tektechnologies.com>
2019-04-17 23:44:46 -0500

    update data in db

--- MillennialResortManager/Database Items/addTestData.bat
+++ MillennialResortManager/Database Items/addTestData.bat
@@ -4,9 +4,10 @@ rem Adds developer written test data to the Database.
 rem 2019-03-22
 
 rem This is the name you change. _____.sql Your file name goes in the blank.
-sqlcmd -S localhost -E -i test_data_scripts/2019-04-10_TEST_Dani.sql
+REM   sqlcmd -S localhost -E -i test_data_scripts/2019-04-10_TEST_Dani.sql
 sqlcmd -S localhost -E -i test_data_scripts/JacobMillerSampleData.sql
-sqlcmd -S localhost -E -i test_data_scripts/Craig.sql
+sqlcmd -S localhost -E -i test_data_scripts/DaniTestData.sql
+
 
 ECHO .
 ECHO If no errors appeared, your test script and/or data was inserted
--- MillennialResortManager/Database Items/master/default_data.sql
+++ MillennialResortManager/Database Items/master/default_data.sql
@@ -55,16 +55,30 @@ INSERT INTO [dbo].[Role]
 	('Worker', 'Gets stuff in, moves stuff out')
 	GO
 	
-INSERT INTO [RoomType]
-(
-    [RoomTypeID], [Description] 
+INSERT INTO [dbo].[RoomType]
+([RoomTypeID],[Description],[Active]
 )	
 VALUES
-    ('Bungalow Land', 'Queen Size Beds.'),
-    ('Bungalow Sea', 'Queen Size Beds'),
-    ('Beach House', 'Sleeps 10 to 20 guests.'),
-	('Royal Suite', 'Kings and Queens, Sleep 2 - 5 guests.'),
-	('Hostel Hut', 'Single Size Beds Sleeps 20 or more.')
+    ('Bungalow Land', 'Queen Size Beds.',1),
+    ('Bungalow Sea', 'Queen Size Beds',1),
+    ('Beach House', 'Sleeps 10 to 20 guests.',1),
+	('Royal Suite', 'Kings and Queens, Sleep 2 - 5 guests.',1),
+	('Hostel Hut', 'Single Size Beds Sleeps 20 or more.',1),
+	('Recreation Center', 'Video Games, Slots, Billiards, Water Park.',1),
+	('Water Park.', 'Includes wave pool and bar.',1),
+	('Shopping Center', 'Local Market Tourists',1),
+	('Food Town', 'Five Star Restuarant',1),
+	('Welcome Wagon', 'Gift Store and Information.',1),
+	('Annex', 'Big Empty Reception Hall.',1)
+
+	
+	
+	
+	
+	
+	
+	
+	
 GO
 
 INSERT INTO [Employee]
