17a537ebdf8dfb43c42be12fb819b846233883d0
Austin Berquam <austinb2015@yahoo.com>
2019-04-18 23:13:11 -0500

    adding views for building, events

--- MillennialResortManager/DataObjects/AppointmentType.cs
+++ MillennialResortManager/DataObjects/AppointmentType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace DataObjects
 {
     public class AppointmentType
     {
-
+        [DisplayName("Service")]
         public string AppointmentTypeID { get; set; }
         public string Description { get; set; }
     }
--- MillennialResortManager/DataObjects/Building.cs
+++ MillennialResortManager/DataObjects/Building.cs
@@ -7,6 +7,7 @@
 /// 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace DataObjects
         public string Name { get; set; }
         public string Address { get; set; }
         public string Description { get; set; }
+        [DisplayName("Current Status")]
         public string StatusID { get; set; }
         public int ResortPropertyID { get; set; }
     }
