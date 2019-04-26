2f8edbf53da42a45e6460de649cbfb71cb1fe4b2
Francis Mingomba <mingomba@gmail.com>
2019-04-26 17:58:16 -0500

    Connected test data

--- MillennialResortManager/Database Items/addTestData.bat
+++ MillennialResortManager/Database Items/addTestData.bat
@@ -6,6 +6,7 @@ rem 2019-03-22
 rem This is the name you change. _____.sql Your file name goes in the blank.
 REM   sqlcmd -S localhost -E -i test_data_scripts/2019-04-10_TEST_Dani.sql
 sqlcmd -S localhost -E -i test_data_scripts/2019-04-04_JamesHeim.sql
+sqlcmd -S localhost -E -i test_data_scripts/2019-04-03FrancisMinomba.sql
 
 REM sqlcmd -S localhost -E -i test_data_scripts/04-10-2019GunardiSaputra.sql
 
--- MillennialResortManager/Database Items/new_sql_additions/2019-04-26FrancisMinomba.sql
+++ MillennialResortManager/Database Items/new_sql_additions/2019-04-26FrancisMinomba.sql
@@ -14,9 +14,4 @@ EXEC sp_create_resort_vehicle_status 'In Use', 'Vehicle currently checked out';
 EXEC sp_create_resort_vehicle_status 'Decomissioned', 'Vehicle dead'                 ;
 EXEC sp_create_resort_vehicle_status 'Available'    , 'Vehicle available for use'    ;
 
-print '' print 'inserting into ResortProperty - REQUIRED'
-EXEC sp_create_resort_property 'Vehicle' ;
-EXEC sp_create_resort_property 'Building' ;
-EXEC sp_create_resort_property 'Room' ;
-
 print '' print 'END FRANCIS MINGOMBA INSERT'
