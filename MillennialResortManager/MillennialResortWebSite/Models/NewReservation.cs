c3a8119a0355c75a52cc048a0102ae22e36a7599
tektechnologies <info@tektechnologies.com>
2019-05-01 13:48:45 -0500

    all forms passing reservation data to complete reservation

--- MillennialResortManager/MillennialResortWebSite/Models/NewReservation.cs
+++ MillennialResortManager/MillennialResortWebSite/Models/NewReservation.cs
@@ -12,12 +12,12 @@ namespace MillennialResortWebSite.Models
         
         [Required]
         [Display(Name = "Start Date")]       
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MMM/yyyy}")]
+        [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:dd/MMM/yyyy}")]
         public DateTime? ArrivalDate { get; set; }
 
         [Required]
         [Display(Name = "End Date")]       
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MMM/yyyy}")]
+        [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:dd/MMM/yyyy}")]
         public DateTime? DepartureDate { get; set; }
 
         [Required]
