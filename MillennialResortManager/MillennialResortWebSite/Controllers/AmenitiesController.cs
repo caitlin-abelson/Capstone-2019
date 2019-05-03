83db4613aee81a52c8a51c2ba1a9cc2d8faeb1ae
Austin Berquam <austinb2015@yahoo.com>
2019-05-03 00:51:00 -0500

    Succes page and times fixed

--- MillennialResortManager/MillennialResortWebSite/Controllers/AmenitiesController.cs
+++ MillennialResortManager/MillennialResortWebSite/Controllers/AmenitiesController.cs
@@ -17,6 +17,46 @@ namespace MillennialResortWebSite.Controllers
         IAppointmentAccessor apptAccessor = new AppointmentAccessorMock();
         IGuestManager _guestManager = new GuestManager();
         IAppointmentManager _apptManager = new AppointmentManager();
+
+        IEnumerable<String> _times;
+
+        public AmenitiesController()
+        {
+            List<string> time = new List<string>{
+                "8:00 am",
+                "8:30 am",
+                "9:00 am",
+                "9:30 am",
+                "10:00 am",
+                "10:30 am",
+                "11:00 am",
+                "11:30 am",
+                "12:00 pm",
+                "12:30 pm",
+                "1:00 pm",
+                "1:30 pm",
+                "2:00 pm",
+                "2:30 pm",
+                "3:00 pm",
+                "3:30 pm",
+                "4:00 pm",
+                "4:30 pm",
+                "5:00 pm",
+                "5:30 pm",
+                "6:00 pm",
+                "6:30 pm"
+            };
+            try
+            {
+                _times = time;
+            }
+            catch (Exception)
+            {
+                // redirect to error page if need be
+                throw;
+            }
+        }
+
         // GET: Amenities
         public ActionResult Index()
         {
@@ -51,7 +91,7 @@ namespace MillennialResortWebSite.Controllers
                 Description = "Incomplete",
                 StartDate = DateTime.Now
             };
-
+            ViewBag.Times = _times;
             return View(appt);
         }
 
@@ -63,12 +103,12 @@ namespace MillennialResortWebSite.Controllers
             {
                 try
                 {
-
                     Guest guest = new Guest();
                     string email = User.Identity.Name;
                     guest = _guestManager.RetrieveGuestByEmail(email);
                     Appointment appt = new Appointment()
                     {
+
                         AppointmentType = appointment.AppointmentType,
                         Description = appointment.Description,
                         StartDate = appointment.StartDate,
@@ -77,12 +117,31 @@ namespace MillennialResortWebSite.Controllers
                     };
                     if (_apptManager.CreateAppointmentByGuest(appt))
                     {
-                        return RedirectToAction("Index", "MyAccount");
+                        TempData["success"] = new SuccessViewModel(
+                            Title: "an Appointment!",
+                            dateTime: appointment.StartDate.ToShortDateString(),
+                            type: appointment.AppointmentType,
+                            time: appointment.Time,
+                            ButtonMessage: "Go to Account",
+                            ReturnController: "MyAccount",
+                            ReturnAction: "Index"
+                    );
+
+                        return RedirectToAction("Index", "Success");
                     }
                 }
-                catch
+                catch (Exception ex)
                 {
-                    throw;
+                    TempData["error"] = new ErrorViewModel(
+                    Title: "Appointment",
+                    Message: "We could not schedule you an appoinment for " + appointment.AppointmentType,
+                    ExceptionMessage: ex.Message,
+                    ButtonMessage: "Back to Amenities",
+                    ReturnController: "Amenitites",
+                    ReturnAction: "Index"
+                    );
+
+                    return RedirectToAction("Index", "Error");
                 }
             }
             return View(appointment);
