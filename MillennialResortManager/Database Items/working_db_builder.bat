db11b8d7b57d9fec45d7fb86f58a7451591ccb64
jaredgreenfield98 <jaredgreenfield98@gmail.com>
2019-05-08 01:20:51 -0500

    Jared: Final DB updates + Presentation Worthy Test Data

--- MillennialResortManager/Database Items/working_db_builder.bat
+++ MillennialResortManager/Database Items/working_db_builder.bat
@@ -4,12 +4,12 @@ rem batch file to run a script to create a db
 rem 2019-02-08
 
 rem Removes the database if it exists
-sqlcmd -S SDSK965\SQLEXPRESS -E -i master\rem_db.sql
+sqlcmd -S localhost -E -i master\rem_db.sql
 rem Use the SQL Studio generated script files to build the working database.
-sqlcmd -S SDSK965\SQLEXPRESS -E -i master\tables.sql
-sqlcmd -S SDSK965\SQLEXPRESS -E -i master\sps.sql
+sqlcmd -S localhost -E -i master\tables.sql
+sqlcmd -S localhost -E -i master\sps.sql
 rem Add required default data for full DB functionality
-sqlcmd -S SDSK965\SQLEXPRESS -E -i master\default_data.sql
+sqlcmd -S localhost -E -i master\default_data.sql
 
 ECHO .
 ECHO if no error messages appear DB was created
